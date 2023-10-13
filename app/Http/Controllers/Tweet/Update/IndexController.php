9f29fe6a52a12e987f12901ad038dcba0db392a2
tetsis <hitoshi.zin.nakao@gmail.com>
2023-10-13 09:42:14 +0900

    first commit

---
+++ app/Http/Controllers/Tweet/Update/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Tweet\Update;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Tweet;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $tweetId = (int)$request->route('tweetId');
+        $tweet = Tweet::where('id', $tweetId)->firstOrFail();
+        return view('tweet.update')->with('tweet', $tweet);
+    }
+}
